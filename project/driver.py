076f24ec46a57c1c020f83c09bb394eacadd48c9
Doris Xu <lx18@rice.edu>
2023-05-02 21:52:40 -0500

    added project2

--- project/driver.py
+++
@@ -1,4 +0,0 @@
-import led_strip as led
-
-strip = led.led_strip()
-strip.start_up_routine()
--- project/pressure_sensor/sensor_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import Adafruit_BBIO.GPIO as GPIO  # import GPIO
-from sensor_driver import HX711  # import the class HX711
-#GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-hx = HX711(dout_pin="P2_4", pd_sck_pin="P2_2")  # create an object
-print(hx.get_raw_data_mean())  # get raw data reading from hx711
-GPIO.cleanup()
